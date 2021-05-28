50a5767330cf3dc48aa4d6437b95d7c122fa2dcb
prabhusiva92 <76937366+prabhusiva92@users.noreply.github.com>
2021-05-28 21:00:09 +0530

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function() {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Testing Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Testing Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+// Email send script
+
+function sendmail(params) {
+    var tempparams = {
+        to_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        from_name: document.getElementById("subject").value,
+        message: document.getElementById("msg").value
+    };
+    emailjs.send("service_pwf3bw5", "template_udje7ya", tempparams).then(function(res) {
+
+        console.log("success", res.status);
+    })
+
+}
